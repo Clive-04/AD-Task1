255cad0f46ef45a0e2acc046945f438e0c537538
Clive-04 <202310384@fit.edu.ph>
2025-05-08 21:27:47 +0800

    fix(added images in catList index)

--- page/catList/index.php
+++ page/catList/index.php
@@ -2,16 +2,17 @@
 <link rel="stylesheet" href="assets/css/catStyle.css">
 <?php
     $cats = [
-        ["name" => "Mittens", "age" => 2, "breed" => "Siamese"],
-        ["name" => "Salmon", "age" => 4, "breed" => "Persian"],
-        ["name" => "Panda", "age" => 2.3, "breed" => "Maine Coon"],
-        ["name" => "Luffy", "age" => 0.8, "breed" => "British Shorthair"],
-        ["name" => "Zoro", "age" => 0.5, "breed" => "Bengal"],
+        ["name" => "Mittens", "age" => 2, "breed" => "Siamese", "img" => "siamese.jpg"],
+        ["name" => "Salmon", "age" => 4, "breed" => "White", "img" => "white.jpg"],
+        ["name" => "Panda", "age" => 1.3, "breed" => "Tabby", "img" => "tabby.jpg"],
+        ["name" => "Luffy", "age" => 0.8, "breed" => "Ginger", "img" => "ginger.jpg"],
+        ["name" => "Zoro", "age" => 0.2, "breed" => "Calico", "img" => "calico.jpg"],
     ];
 
     echo "<div class='cat-list'>";
     foreach ($cats as $cat) {
         echo "<div class='cat-card'>";
+        echo "<img src='assets/img/{$cat['img']}' alt='{$cat['name']}' class='cat-img'><br>";
         echo "<strong>{$cat['name']}</strong><br>Age: {$cat['age']} years<br>";
         echo "Breed: {$cat['breed']}<br>";
 
