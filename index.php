728daa3c7b1778b0df1f5b5f05890379b67db016
Clive-04 <202310384@fit.edu.ph>
2025-05-08 20:56:58 +0800

    fix(configured global css for better appearance and suitability)

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
 
 <link rel="stylesheet" href="assets/css/global.css">
 <div class="landing">
-    <h1>Welcome to <?= $shelterName?>!</h1>
-    <p>Your trusted place to find loving feline companions.</p>
-    <p><strong>Location: </strong><?= $location ?></p>
     <img src="assets/img/cat banner.jpg" alt="Cat Banner" class="landing-img">
-    <a href="./page/catList/index.php" class="btn">View Available Cats</a>
+    <div class="landing-text">
+        <h1>Welcome to <?= $shelterName?>!</h1>
+        <p>Your trusted place to find loving feline companions.</p>
+        <p><strong>Location: </strong><?= $location ?></p>
+        <a href="./page/catList/index.php" class="btn">View Available Cats</a>
+    </div>
 </div>
 <script src="assets/js/global.js">
