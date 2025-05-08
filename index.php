1e625d939c7c12e0214199fbe61b7634f5e7f2b4
Clive-04 <202310384@fit.edu.ph>
2025-05-08 20:49:33 +0800

    feat(added image for better design)

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <h1>Welcome to <?= $shelterName?>!</h1>
     <p>Your trusted place to find loving feline companions.</p>
     <p><strong>Location: </strong><?= $location ?></p>
-    <img src="assets/img/cat-banner.jpg" alt="Cat Banner" class="landing-img">
+    <img src="assets/img/cat banner.jpg" alt="Cat Banner" class="landing-img">
     <a href="./page/catList/index.php" class="btn">View Available Cats</a>
 </div>
 <script src="assets/js/global.js">
