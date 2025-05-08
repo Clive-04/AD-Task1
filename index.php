9dba248e7bef3ba22f25107a2b8aff3b4108b81f
Clive-04 <202310384@fit.edu.ph>
2025-05-08 21:08:09 +0800

    fix(changed font style and image, added another css config for the text)

--- index.php
+++ index.php
@@ -9,9 +9,11 @@
     <img src="assets/img/cat banner.jpg" alt="Cat Banner" class="landing-img">
     <div class="landing-text">
         <h1>Welcome to <?= $shelterName?>!</h1>
+    </div>
+    <div class="p-text">
         <p>Your trusted place to find loving feline companions.</p>
         <p><strong>Location: </strong><?= $location ?></p>
-        <a href="./page/catList/index.php" class="btn">View Available Cats</a>
     </div>
+    <a href="./page/catList/index.php" class="btn">View Available Cats</a>
 </div>
 <script src="assets/js/global.js">
