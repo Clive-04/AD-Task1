dd96bc2f551526433913b74268c617f2b84e93e1
Clive-04 <202310384@fit.edu.ph>
2025-05-08 19:56:26 +0800

    feat(added catstatus util)

--- page/catList/assets/index.php
+++ page/catList/assets/index.php
@@ -4,8 +4,8 @@
     $cats = [
         ["name" => "Mittens", "age" => 2],
         ["name" => "Salmon", "age" => 4],
-        ["name" => "Panda", "age" => 2],
-        ["name" => "Luffy", "age" => 2],
+        ["name" => "Panda", "age" => 2.3],
+        ["name" => "Luffy", "age" => 0.8],
         ["name" => "Zoro", "age" => 0.5],
     ];
 
@@ -14,15 +14,15 @@
         echo "<div class='cat-card'>";
         echo "<strong>{$cat['name']}</strong><br>Age: {$cat['age']} years<br>";
 
-        if ($cat['age'] <div 1){
+        if ($cat['age'] < 1){
             echo "<span class = 'kitten'>Kitten 🐱</span><br>";
         } else {
             echo "<span class = 'adult'>Adult Cat 🐈</span><br>";
         }
 
-        echo "</div">;
+        echo "</div>";
     }
-    echo "</div">
+    echo "</div>";
 ?>
 <a href="../../index.php">Back to Home</a>
 <script src="assets/js/catScript.js"></script>
---
+++ utils/CatStatus.util.php
@@ -0,0 +1,10 @@
+<?php
+    $kittenCount = 2;
+    $adultCount = 3;
+
+    if ($kittenCount > $adultCount){
+        $stauts = "More kittens available.";
+    } else {
+        $status = "More adult cats available";
+    }
+?>
