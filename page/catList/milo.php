2f51af34fb281d07ce10ce1796ceeb8f69195d53
Clive-04 <202310384@fit.edu.ph>
2025-05-10 01:17:38 +0800

    feat(added another profile)

---
+++ page/catList/milo.php
@@ -0,0 +1,19 @@
+<?php include(__DIR__."/../../components/header.component.php"); ?>
+<link rel="stylesheet" href="/../assets/css/global.css">
+<link rel="stylesheet" href="assets/css/profileStyle.css">
+
+<div class="cat-profile">
+    <h1>Milo's Profile</h1>
+    <img src="assets/img/tabby.jpg" alt="Milo" class="profile-img">
+    <p><strong>Age:</strong> 1.3 years</p>
+    <p><strong>Breed:</strong> Tabby</p>
+    <p><strong>Personality:</strong> Friendly and Laid-back</p>
+    <p><strong>Hobbies: </strong> Window watching, especially birds</p>
+    <p><strong>Likes: </strong> Cozy blankets, baskets, or your laundry pile</p>
+    <p><strong>Dislikes: </strong> Being picked up too much and empty food bowls</p>
+    <p><strong>Status:</strong> Adult Cat 🐈</p>
+</div>
+
+<div class="back-button-wrapper">
+    <a href="index.php" class="btn">Back to Cat List</a>
+</div>
