0fe7f2e3061dd807b33967a3ad9b7a79bfffab17
Clive-04 <202310384@fit.edu.ph>
2025-05-09 17:53:16 +0800

    fix(added slug to redirect to other php file)

--- page/catList/index.php
+++ page/catList/index.php
@@ -5,17 +5,19 @@
 
 <?php
     $cats = [
-        ["name" => "Mittens", "age" => 2, "breed" => "Siamese", "img" => "siamese.jpg"],
-        ["name" => "Snow", "age" => 4, "breed" => "White", "img" => "white.jpg"],
-        ["name" => "Milo", "age" => 1.3, "breed" => "Tabby", "img" => "tabby.jpg"],
-        ["name" => "Luffy", "age" => 0.5, "breed" => "Ginger", "img" => "ginger.jpg"],
-        ["name" => "Zoro", "age" => 0.2, "breed" => "Calico", "img" => "calico.jpg"],
+        ["name" => "Mittens", "age" => 2, "breed" => "Siamese", "img" => "siamese.jpg", "slug" => "Mittens"],
+        ["name" => "Snow", "age" => 4, "breed" => "White", "img" => "white.jpg", "slug" => "Snow"],
+        ["name" => "Milo", "age" => 1.3, "breed" => "Tabby", "img" => "tabby.jpg", "slug" => "Milo"],
+        ["name" => "Luffy", "age" => 0.5, "breed" => "Ginger", "img" => "ginger.jpg", "slug" => "Luffy"],
+        ["name" => "Zoro", "age" => 0.2, "breed" => "Calico", "img" => "calico.jpg", "slug" => "Zoro"],
     ];
 
     echo "<div class='cat-list'>";
     foreach ($cats as $cat) {
         echo "<div class='cat-card'>";
+        echo "<a href='../../page/profile/{$cat['slug']}.php'>";
         echo "<img src='assets/img/{$cat['img']}' alt='{$cat['name']}' class='cat-img'><br>";
+        echo "</a>";
         echo "<strong>{$cat['name']}</strong><br>Age: {$cat['age']} years<br>";
         echo "Breed: {$cat['breed']}<br>";
 
