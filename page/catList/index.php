48ece4af5ce25911ac7a0a95934a73075fce1750
Clive-04 <202310384@fit.edu.ph>
2025-05-09 23:37:29 +0800

    fix(added code under foreach section)

--- page/catList/index.php
+++ page/catList/index.php
@@ -5,28 +5,31 @@
 
 <?php
     $cats = [
-        "mittens" => ["name" => "Mittens", "age" => 2, "breed" => "Siamese", "img" => "siamese.jpg", "slug" => "mittens"],
-        "snow" => ["name" => "Snow", "age" => 4, "breed" => "White", "img" => "white.jpg", "slug" => "snow"],
-        "milo" => ["name" => "Milo", "age" => 1.3, "breed" => "Tabby", "img" => "tabby.jpg", "slug" => "milo"],
-        "luffy" => ["name" => "Luffy", "age" => 0.5, "breed" => "Ginger", "img" => "ginger.jpg", "slug" => "luffy"],
-        "zoro" => ["name" => "Zoro", "age" => 0.2, "breed" => "Calico", "img" => "calico.jpg", "slug" => "zoro"],
+        ["name" => "Mittens", "age" => 2, "breed" => "Siamese", "img" => "siamese.jpg"],
+        ["name" => "Snow", "age" => 4, "breed" => "White", "img" => "white.jpg"],
+        ["name" => "Milo", "age" => 1.3, "breed" => "Tabby", "img" => "tabby.jpg"],
+        ["name" => "Luffy", "age" => 0.5, "breed" => "Ginger", "img" => "ginger.jpg"],
+        ["name" => "Zoro", "age" => 0.2, "breed" => "Calico", "img" => "calico.jpg"],
     ];
 
     echo "<div class='cat-list'>";
     foreach ($cats as $cat) {
+        $query = http_build_query([
+            'name' => $cat['name'],
+            'age' => $cat['age'],
+            'breed' => $cat['breed'],
+            'img' => $cat['img']
+        ]);
         echo "<div class='cat-card'>";
-        echo "<a href='profile.php?cat=".strtolower($cat['slug'])."'>";
         echo "<img src='assets/img/{$cat['img']}' alt='{$cat['name']}' class='cat-img'><br>";
-        echo "</a>";
         echo "<strong>{$cat['name']}</strong><br>Age: {$cat['age']} years<br>";
         echo "Breed: {$cat['breed']}<br>";
-
+    
         if ($cat['age'] < 1){
-            echo "<span class = 'kitten'>Kitten 🐱</span><br>";
+            echo "<span class='kitten'>Kitten 🐱</span><br>";
         } else {
-            echo "<span class = 'adult'>Adult Cat 🐈</span><br>";
+            echo "<span class='adult'>Adult Cat 🐈</span><br>";
         }
-
         echo "</div>";
     }
     echo "</div>";
