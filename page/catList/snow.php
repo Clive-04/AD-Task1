c14b1296069a4d133696b2aa2968add42f92ba60
Clive-04 <202310384@fit.edu.ph>
2025-05-10 01:13:54 +0800

    feat(added another profile)

---
+++ page/catList/snow.php
@@ -0,0 +1,19 @@
+<?php include(__DIR__."/../../components/header.component.php"); ?>
+<link rel="stylesheet" href="/../assets/css/global.css">
+<link rel="stylesheet" href="assets/css/profileStyle.css">
+
+<div class="cat-profile">
+    <h1>Snow's Profile</h1>
+    <img src="assets/img/white.jpg" alt="Snow" class="profile-img">
+    <p><strong>Age:</strong> 4 years</p>
+    <p><strong>Breed:</strong> White</p>
+    <p><strong>Personality:</strong> Observant</p>
+    <p><strong>Hobbies: </strong> Light play, especially with soft or feathery toys</p>
+    <p><strong>Likes: </strong> Natural light and warm napping spots</p>
+    <p><strong>Dislikes: </strong> Getting dirty</p>
+    <p><strong>Status:</strong> Adult Cat 🐈</p>
+</div>
+
+<div class="back-button-wrapper">
+    <a href="index.php" class="btn">Back to Cat List</a>
+</div>
