ee0488f3e132bc660531bdc26abe161ab5222c29
Clive-04 <202310384@fit.edu.ph>
2025-05-10 01:23:36 +0800

    feat(added the fourth subject profile)

---
+++ page/catList/luffy.php
@@ -0,0 +1,19 @@
+<?php include(__DIR__."/../../components/header.component.php"); ?>
+<link rel="stylesheet" href="/../assets/css/global.css">
+<link rel="stylesheet" href="assets/css/profileStyle.css">
+
+<div class="cat-profile">
+    <h1>Luffy's Profile</h1>
+    <img src="assets/img/ginger.jpg" alt="Luffy" class="profile-img">
+    <p><strong>Age:</strong> 5 months</p>
+    <p><strong>Breed:</strong> Ginger</p>
+    <p><strong>Personality:</strong> Mischievous</p>
+    <p><strong>Hobbies: </strong> Playing a lot</p>
+    <p><strong>Likes: </strong> Belly rubs, Tasty treats, Sunbathing</p>
+    <p><strong>Dislikes: </strong> Delayed mealtime, Baths, Closed doors</p>
+    <p><strong>Status:</strong> Kitten 🐱</p>
+</div>
+
+<div class="back-button-wrapper">
+    <a href="index.php" class="btn">Back to Cat List</a>
+</div>
