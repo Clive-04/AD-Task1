542874c15ae7b829be959149a36c2c4af91d9662
Clive-04 <202310384@fit.edu.ph>
2025-05-12 17:15:50 +0800

    feat (added logo on the header component)

--- components/header.component.php
+++ components/header.component.php
@@ -1,4 +1,4 @@
-<header>
-    <h1>🐾 Happy Paws Shelter</h1>
-    <hr>
+<header class="site-header">
+    <img src="assets/img/logo.png" alt="Happy Paws Logo" class="logo">
+    <h1>Happy Paws Shelter</h1>
 </header>
